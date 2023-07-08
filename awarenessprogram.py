3de7dcaa512a58c905da7d9db22c74cdc418d268
Nik14329 <112238414+Nik14329@users.noreply.github.com>
2023-07-08 12:41:29 +0530

    Create awarenessprogram.py

---
+++ awarenessprogram.py
@@ -0,0 +1,196 @@
+##python code for awareness programme##
+print("WELCOME TO THE COVID-19 AWARENESS SURVEY") print("PERSONAL DETAILS:-")
+name=input ("Enter your name: ")
+age= int(input("Enter your age: "))
+gender=input ("Specify your gender: ")
+print ("Since this is a covid related quiz/survey/awareness program, please answer the following questions. (Answer in caps/numbers as mentioned): ") print(" ")
+a=input ("Q.HAVE YOU TAKEN THE PCR TEST FOR COVID-19 ANYTIME AFTER THE START OF THE PANDEMIC? (YES/NO) \nAns: ")
+if a "YES":
+b=input ("Q. WERE YOU TESTED +VE OR -VE? (P/N) \nAns: ")
+if b=="p":
+else:
+g-input("Q. What symptoms are you having right now?\nAns: ")
+h-input ("Q. How long have you been facing these symptoms?\nAns: ")
+i-input ("Q.How long has the treatment been going on?\nAns: ")
+j-input ("Q. What have you been doing in quarantine to gain motivation?\nAns: ") k-input ("Q. Can you name some variants of coronavirus?\nAns: ")
+print ("Dear Friend, please take care of the following things")
+print("
+\n")
+print ("We know this is a hard time but it shall pass soon!!!\n") print ("1) Use a thermometer to keep a track on the body Temperature \n2) Use the oximeter's pulse reading to keep a track on the pulse\n3) Try to sleep on stomach & avoid lying on the b print ("Take care!")
+c=input ("Q.WERE ANY OF YOUR FAMILY MEMBER TESTED POSITIVE/ HAD COVID? (YES/NO)\nAns: ")
+if c=="YES":
+gl-input ("Q. What symptoms are they having right now?\nAns: ")
+h1-input ("Q. How long have they been facing these symptoms?\nAns: ")
+11-input ("Q. How long has the treatment been going on?\nAns: ")
+j1-input ("Q. What have you been doing in quarantine to gain motivation?\nAns: ") kl-input ("Q.can you name some variants of coronavirus?\nAns: ") print ("Dear Friend, please take care of the following things") print("
+print ("We know this is a hard time but it shall pass soon!!!\n")
+\n")
+print ("1) Use a thermometer to keep a track on the body Temperature \n2) Use the oximeter's pulse reading to keep a track on the pulse \n3) Try to sleep on stomach & avoid lying on t print ("Take care!")
+else:
+d-input ("Q.Are you vaccinated? (YES/NO) \nAns: ")
+if d=="YES":
+x-input ("Please enter the name of the vaccine taken (Covishield/Covaxin):")
+
+x=input ("Please enter the name of the vaccine taken (Covishield/Covaxin):") y=input ("Please state the post vaccine symptoms experienced (if multiple answers, seperate using comma (,):") 2=int (input ("Please enter number of recovery days required post the vaccine (in numbers only):"))
+strl=''
+Thank you for being a responsible citizen of the country and taking your assigned dose of the vaccine early on. However, the pandemic still continues to target mankind and during these touc print (strl)
+DIET
+#diet str3=***
+It is important to have a nutrition rich diet to support our immunity consisting of:
+1. Protiens- Eggs, chicken and other types of meats, Fulses (Lentils, Beans, etc), Nuts (Almonds, Peanuts, etc.), Cereals (Oats, Wheat, etc.), Paneer/Tofu.
+2. Fibre- Raw vegetables and fruits in salads or whole, seasonal fruits (Mango, Watermelon, etc.) & citrus fruits rich in vitamin c.
+3. Fats- Unsaturated fats (Fishes, Soy) and cooking oils with unsaturated fats (olive oil, Sunflower oil)
+4. Vitamins- Various vitamins like A(Papaya, Carrots), Bcomplex (Milk, Eggs, Meat, Legumes, etc.), C(Citrus, Amla), D(Basking in sunlight, Salmon, etc.), E (Various nuts and seeds), K(Green leafy vege
+5. Other elemental components- Food rich in Iron (Cashews, Lentils, etc.), zinc (Seeds, Meat, Etc.), Potassium (Bananas, Beetroots, etc.), Calcium (Milk, Yogurt, etc.)
+It is advisable to:
+1. Avoid outside foods.
+2. Avoid salty foods
+3. Consume healthy homecooked food. ***
+print (str3)
+#physical and mental wellbeing str4=
+PHYSICAL AND MENTAL WELLBEING
+1. Sleep- Sleeping for 7-8 hours everyday helps the immune system.
+2. Hydrate- Drink atleast 8 glasses of water in a day.
+3. Exercise- Along with a good diet, exercising daily does wonders for the immunity.
+4. Meditation- Practice Yoga for destressing and building up your immunity.
+5. Less Panic, More Action- Even if you contract the virus, remember that more than 80% of the patients recover without any complications. So, do not panic and take the necessary measure accc 6. Rest- Take appropriate amount of rest to unwind and relax after excessive screentime.** print (str4)
+else:
+sl=input("Q.DO YOU GET TIRED EASILY? (YES/NO) \nAns: ") $2-input ("Q.DO YOU GET FEVER OR CHILLS? (YES/NO) \nAns: ")
+sl=input ("Q.DO YOU GET TIRED EASILY? (YES/NO) \nAns: ") $2-input ("Q.DO YOU GET FEVER OR CHILLS? (YES/NO) \nAns: ")
+s3-input ("Q.DO YOU HAVE DIFFICULTY IN BREATHING? (YES/NO) \nAns: ") s4=input("Q.DO YOU FEEL LOSS OF SENSE OF SMELL OR TASTE? (YES/NO) \nAns: ")
+diet1="When a person is sick, they may find it difficult to develop an appetite. \n However, it is important to receive nourishment and stay hydrated, \n especially when feel diet2="Eat a variety of foods: \nEvery day, eat a combination of different foods including whole grains \nsuch as wheat, maize and rice, \nlegumes like lentils and beans, fru diet3="Be strategic about the use of ingredients prioritize fresh products \nPrepare home-cooked meals \nFollow safe food handling practices \nLimit your salt intake \nLin
+if (s1="YES" and $2=="YES"):
+if (s3"YES" and s4=="YES"):
+print("PLEASE VISIT A DOCTOR AND GET TESTED IMMEDIATELY. TAKE CARE AND STAY SAFE 0")
+elif (s3="NO" and s4=="YES"):
+print (diet1)
+elif (s3"YES" and s4=="NO"):
+print (diet2)
+else:
+print (diet3)
+elif (sl=="NO" and s2=="NO"):
+if (s3="YES" and s4=="YES"):
+print ("PLEASE VISIT A DOCTOR AND GET TESTED IMMEDIATELY. TAKE CARE AND STAY SAFE Q")
+elif (s3="NO" and s4=="YES"):
+print (diet1)
+elif (s3=="YES" and s4=="NO"):
+print (diet2)
+else:
+print (diet3)
+elif (s1="YES" and $2=="NO"):
+if (s3"YES" and s4=="YES"):
+print("PLEASE VISIT A DOCTOR AND GET TESTED IMMEDIATELY. TAKE CARE AND STAY SAFE O")
+elif (s3"NO" and $4=="YES"):
+print (diet1)
+elif (s3"YES" and s4=="NO"):
+print (diet2)
+else:
+print (diet3)
+elif (sl="NO" and $2=="YES"):
+if (s3=="YES" and $4=="YES"):
+print("PLEASE VISIT A DOCTOR AND GET TESTED IMMEDIATELY. TAKE CARE AND STAY SAFE 0")
+elif (s3="NO" and $4=="YES"):
+print (diet1)
+elif (s3"YES" and s4=="NO"):
+print (diet2)
+else:
+print (diet)
+
+e-input ("Q.WERE ANY OF YOUR FAMILY MEMBER TESTED POSITIVE/ HAD COVID? (YES/NO)\nAns: ") if e="y":
+else:
+g2=input ("Q. What symptoms are they having right now?\nAns: ")
+h2-input ("Q. How long have they been facing these symptoms?\nAns: ")
+12-input ("Q. How long has the treatment been going on?\nAns: ")
+12-input ("Q. What have you been doing in quarantine to gain motivation?\nAns: ") k2=input ("Q. Can you name some variants of coronavirus?\nAns: ")
+print ("Dear Friend, please take care of the following things") print("
+print("we know this is a hard time but it shall pass soon!!!\n")
+\n")
+print ("1) Use a thermometer to keep a track on the body Temperature \n2) Use the oximeter's pulse reading to keep a track on the pulse \n3) Try to sleep on stomach avoid lying on the I print ("Take care!")
+f-input ("Q.Are you vaccinated? (YES/NO) \nAns: ")
+if f=="YES":
+x1=input ("Please enter the name of the vaccine taken (Covishield/Covaxin):")
+yl=input ("Please state the post vaccine symptoms experienced (if multiple answers, seperate using comma (,):")
+zl=int(input("Please enter number of recovery days required post the vaccine (in numbers only):"))
+str5=***
+Thank you for being a responsible citizen of the country and taking your assigned dose of the vaccine early on. However, the pandemic still continues to target mankind and during these toug print (str5)
+DIET
+#diet str6=***
+It is important to have a nutrition rich diet to support our immunity consisting of:
+1. Protiens- Eggs, Chicken and other types of meats, Pulses (Lentils, Beans, etc), Nuts (Almonds, Peanuts, etc.), Cereals (Oats, Wheat, etc.), Paneer/Tofu.
+2. Fibre- Raw vegetables and fruits in salads or whole, seasonal fruits (Mango, Watermelon, etc.) & Citrus fruits rich in Vitamin C.
+3. Fats- Unsaturated fats (Fishes, Soy) and cooking oils with unsaturated fats (olive oil, Sunflower oil)
+4. vitamins- various vitamins like A (Papaya, Carrots), Bcomplex (Milk, Eggs, Meat, Legumes, etc.), C(citrus, Amla), D(Basking in sunlight, salmon, etc.), E(various nuts and seeds), K(Green leafy vege 5. Other elemental components- Food rich in Iron (Cashews, Lentils, etc.), Zinc (Seeds, Meat, Etc.), Potassium (Bananas, Beetroots, etc.), Calcium (Milk, Yogurt, etc.)
+It is advisable to:
+1. Avoid outside foods.
+2. Avoid salty foods
+3. Consume healthy homecooked food.'*'
+
+3. Consume healthy homecooked food. ''' print (str6)
+#physical and mental wellbeing str7=***
+PHYSICAL AND MENTAL WELLBEING
+1. Sleep- Sleeping for 7-8 hours everyday helps the immune system.
+2. Hydrate- Drink atleast 8 glasses of water in a day.
+3. Exercise- Along with a good diet, exercising daily does wonders for the immunity.
+4. Meditation- Practice Yoga for destressing and building up your immunity.
+5. Less Panic, More Action- Even if you contract the virus, remember that more than 80% of the patients recover without any complications. So, do not panic and take the necessary measure accc 6. Rest- Take appropriate amount of rest to unwind and relax after excessive screentime.'**
+print (str7)
+else:
+syl-input ("Q.DO YOU GET TIRED EASILY? (YES/NO) \nAns: ")
+sy2-input ("Q. DO YOU GET FEVER OR CHILLS? (YES/NO) \nAns: ")
+sy3-input ("Q.DO YOU HAVE DIFFICULTY IN BREATHING? (YES/NO) \nAns: ")
+sy4=input ("Q. DO YOU FEEL LOSS OF SENSE OF SMELL OR TASTE? (YES/NO) \nAns: ")
+d1="when a person is sick, they may find it difficult to develop an appetite. \n However, it is important to receive nourishment and stay hydrated, in especially when feeling unw d2="Eat a variety of foods: \nEvery day, eat a combination of different foods including whole grains \nsuch as wheat, maize and rice, \nlegumes like lentils and beans, fruit and d3="Be strategic about the use of ingredients prioritize fresh products \nPrepare home-cooked meals \nFollow safe food handling practices \nLimit your salt intake \nLimit your
+if (syl=="YES" and sy2=="YES"):
+if (sy3=="YES" and sy4=="YES"):
+print("PLEASE VISIT A DOCTOR AND GET TESTED IMMEDIATELY. TAKE CARE AND STAY SAFE Q")
+elif (sy3=="NO" and sy4=="YES"):
+print (d1)
+elif (sy3=="YES" and sy4=="NO"): print (d2)
+else:
+print (d3)
+elif (syl=="NO" and sy2=="NO"):
+if (sy3=="YES" and sy4=="YES"):
+print ("PLEASE VISIT A DOCTOR AND GET TESTED IMMEDIATELY. TAKE CARE AND STAY SAFE 0")
+elif (sy3="NO" and sy4=="YES"):
+print (d1)
+elif (sy3=="YES" and sy4=="NO"): print (d2)
+else:
+print (d3)
+elif (syl="YES" and sy2=="NO"):
+if (sy3=="YES" and sy4=="YES"):
+print("PLEASE VISIT A DOCTOR AND GET TESTED IMMEDIATELY. TAKE CARE AND STAY SAFE 0") elif (sy3=="NO" and sy4=="YES"):
+print (d1)
+elif (sy3=="YES" and sy4=="NO"):
+else: print (d3)
+elif (syl="NO" and sy2=="NO"):
+if (sy3=="YES" and sy4=="YES"):
+print ("PLEASE VISIT A DOCTOR AND GET TESTED IMMEDIATELY. TAKE CARE AND STAY SAFE 0")
+elif (sy3=="NO" and sy4=="YES"):
+print (d1)
+elif (sy3=="YES" and sy4=="NO"): print (d2)
+else:
+print (d3)
+elif (syl="YES" and sy2=="NO"):
+if (sy3"YES" and sy4=="YES"):
+print ("PLEASE VISIT A DOCTOR AND GET TESTED IMMEDIATELY. TAKE CARE AND STAY SAFE O")
+elif (sy3="NO" and sy4=="YES"):
+print (d1)
+elif (sy3=="YES" and sy4=="NO"): print (d2)
+else:
+print (d3)
+elif (syl=="NO" and sy2=="YES"):
+if (sy3="YES" and sy4=="YES"):
+print ("PLEASE VISIT A DOCTOR AND GET TESTED IMMEDIATELY. TAKE CARE AND STAY SAFE O")
+elif (sy3=="NO" and sy4=="YES"):
+print (d1)
+elif (sy3"YES" and sy4=="NO"):
+else:
+print (d2)
+print (d3)
+print ("
+print ("Prevention methods: ")
+print ("To prevent the spread of COVID-19: \nclean your hands often. \nuse soap and water, or an alcohol-based hand rub. \nMaintain a safe distance from anyone who is coughing or sneezing. \nw print ("Alongside the safety measures, to help follow Covid-19 social distancing guidelines, \nhere is a little contribution towards maintaining safety distance. \nThe Covid Safety Sensor") print("THANKYOU FOR YOUR VALUABLE TIME")
+p=input (" (Press any key to continue)")
+import webbrowser
+webbrowser.open("https://www.tinkercad.com/embed/fk2HKmhkMFE?editbtn=1")
+***********************************************************************************************************************************************************************************************************
